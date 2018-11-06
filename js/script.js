19f24f023cfff25fce3ef67f32e99c62b0654c4c
Noelia Vaz <nlvazquez01@gmail.com>
2018-11-06 08:51:24 -0500

    complete appendPageLinks functionality and changed comments

--- js/script.js
+++ js/script.js
@@ -3,39 +3,14 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-
+//global variables that selects the ul that holds the students
+//and the actual collection of students
 const ul = document.querySelector('.student-list');
 const lis = ul.children;
 
 
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
 
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-
+//showPage function that displays appropriate students
 const showPage = (lis, pageNum) => {
 	for (let i = 0; i < lis.length; i++)
 		if(i >= (pageNum-1)*10 && i < pageNum*10)
@@ -45,12 +20,7 @@ const showPage = (lis, pageNum) => {
 }
 
 
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
+//appendPageLinks function that creates buttons and calls showPage function
 const appendPageLinks = lis => {
 	const pageNums = Math.ceil(lis.length/10);
 	const studentDiv = document.querySelector('.page');
@@ -72,12 +42,16 @@ const appendPageLinks = lis => {
 		pageLI.appendChild(pageAnchor);
 
 		pageAnchor.addEventListener('click', (e) => {
-			showPage(lis, parseInt(e.target.textContent));
+			const allAnchors = document.querySelectorAll(".pagination a");
+			const targetAnchor = e.target;
+
+			showPage(lis, parseInt(targetAnchor.textContent));
+			for(let a = 0; a < allAnchors.length; a++) {
+				allAnchors[a].classList.remove('active');
+			}
+			targetAnchor.classList.add('active');
 		})
 	}
 }
 
 appendPageLinks(lis);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
